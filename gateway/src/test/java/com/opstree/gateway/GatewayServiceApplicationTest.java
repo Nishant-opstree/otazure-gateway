c74bd3b587ca52282a53f5e481c6d7db317679aa
Nishant <nishant.prakash@opstree.com>
2020-08-11 06:55:31 +0530

    upload

---
+++ gateway/src/test/java/com/opstree/gateway/GatewayServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package com.mkyong.examples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GatewayServiceApplicationTest {
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals("hello world", "hello world");
+    }
+
+}
